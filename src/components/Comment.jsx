33571cd328ef9f9db391960f581a573b0f1fdc69
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-12-20 17:04:19 -0300

    Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import styles from "./Comment.module.css"
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/rodrigocan.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Rodrigo Cândido</strong>
+              <time
+                title="3 de Agosto às 09:52h"
+                dateTime="2022-08-03 09:52:31"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
