ce5b41bd2d248970f6c624562dedc0e8ef13afbf
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-12-19 21:53:23 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from "./Post"
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Diego Fernandes"
+        content="Ut enim sint aliquip ea consequat cupidatat qui excepteur dolor aliquip id magna sint deserunt. Reprehenderit pariatur sunt do consectetur id excepteur. Officia amet reprehenderit consequat ea eiusmod reprehenderit culpa do."
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Ut cillum incididunt reprehenderit quis ex quis do sit. Sint commodo cupidatat nostrud do reprehenderit aliqua ipsum amet consectetur enim laborum. Ut fugiat nisi ad consequat do in enim ut."
+      />
     </div>
   )
 }
