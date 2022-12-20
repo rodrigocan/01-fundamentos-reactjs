a389bca637a2a71163ec32a470c09d9711efdd97
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-12-20 11:05:11 -0300

    Componente: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,49 @@
+import styles from "./Post.module.css"
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/rodrigocan.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Rodrigo Cândido</strong>
+            <span>Software Engineer</span>
+          </div>
+        </div>
+
+        <time title="3 de Agosto às 09:52h" dateTime="2022-08-03 09:52:31">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Irure voluptate sit magna ullamco 👋</p>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem iste
+          provident autem in qui modi fuga id perferendis, ad alias eos at
+          voluptates ratione esse enim corporis quo deserunt facere.
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eos
+          omnis nobis, nesciunt dolores rerum vel minus soluta eaque eum enim
+          molestiae ratione nulla, unde maxime! Pariatur commodi tenetur
+          deleniti.
+        </p>
+
+        <p>
+          🔗<a href="">https://www.github.com/rodrigocan</a>
+        </p>
+
+        <p>
+          <a href="">#project #react #javascript</a>
+        </p>
+      </div>
+    </article>
+  )
+}
