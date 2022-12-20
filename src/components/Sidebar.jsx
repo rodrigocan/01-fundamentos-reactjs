f54ad8a5d4e66d265eec2f98340837482419c923
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-12-20 18:32:51 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react"
+import { Avatar } from "./Avatar"
 
 import styles from "./Sidebar.module.css"
 
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/rodrigocan.png"
-        />
+        <Avatar src="https://github.com/maykbrito.png" />
 
         <strong>Rodrigo Cândido</strong>
         <span>Software Engineer</span>
