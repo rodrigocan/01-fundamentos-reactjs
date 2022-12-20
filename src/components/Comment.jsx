d67d441330c06edfe987d914960df4378ad01281
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-12-20 17:19:28 -0300

    Estilizações do Comentário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -20,7 +20,7 @@ export function Comment() {
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
 
