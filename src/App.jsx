00803e9d75ffe9bb2d1ddf1dea278ea39ebae0c5
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-12-20 10:29:31 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from "./App.module.css"
+
 import "./global.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Diego Fernandes"
-        content="Ut enim sint aliquip ea consequat cupidatat qui excepteur dolor aliquip id magna sint deserunt. Reprehenderit pariatur sunt do consectetur id excepteur. Officia amet reprehenderit consequat ea eiusmod reprehenderit culpa do."
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Ut cillum incididunt reprehenderit quis ex quis do sit. Sint commodo cupidatat nostrud do reprehenderit aliqua ipsum amet consectetur enim laborum. Ut fugiat nisi ad consequat do in enim ut."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Ut enim sint aliquip ea consequat cupidatat qui excepteur dolor aliquip id magna sint deserunt. Reprehenderit pariatur sunt do consectetur id excepteur. Officia amet reprehenderit consequat ea eiusmod reprehenderit culpa do."
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Ut cillum incididunt reprehenderit quis ex quis do sit. Sint commodo cupidatat nostrud do reprehenderit aliqua ipsum amet consectetur enim laborum. Ut fugiat nisi ad consequat do in enim ut."
+          />
+        </main>
+      </div>
     </div>
   )
 }
